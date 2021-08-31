d15ac0d45088bdc61b01a535c794eedd6b6326f9
Vu Ngoc Thinh <vnt462@gmail.com>
2021-08-31 18:21:45 +0700

    create track creating screen.

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
-
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Map from '../components/Map';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
+import '../_mockLocation';
 
 const TrackCreateScreen = () => {
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+            await watchPositionAsync(
+                {
+                    accuracy: Accuracy.BestForNavigation,
+                    timeInterval: 1000,
+                    distanceInterval: 10
+                }, (location) => {
+                    console.log(location);
+                });
+        } catch (error) {
+            setErr(error);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
-        <View>
-            <Text style={{fontSize: 48}}>TrackCreateScreen</Text>
-        </View>
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h2>Create a Track</Text>
+            <Map></Map>
+            {err ? <Text>Please enable location services</Text> : null}
+        </SafeAreaView>
     );
 };
 
